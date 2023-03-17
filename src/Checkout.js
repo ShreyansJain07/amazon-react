4e0c4908e68efc48e108df89f32597af5c719c54
Shreyans jain <shreyansjain1603@gmail.com>
2023-03-17 15:11:28 +0530

    commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,23 +1,46 @@
-import React from 'react'
-import "./Checkout.css"
-import Subtotal from './Subtotal'
-import { useStateValue } from './StateProvider'
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 const Checkout = () => {
-    const [{basket},dispatch] = useStateValue();
-    return (
-    <div className='checkout'>
-    <div className='checkout__left'>
-        <div className='checkout__title'>
-            <h2>Shopping Cart</h2>
-            {basket.map((product) => <div>{product.title}</div>)}
+  const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <div className="checkout__title">
+          <h2>Shopping Cart</h2>
         </div>
-    </div>
-    <div className='checkout__right'>
+        {basket.length !== 0 ? (
+          <>
+            {basket.map((product) => (
+              <CheckoutProduct
+                id={product.id}
+                title={product.title}
+                rating={product.rating}
+                price={product.price}
+                image={product.image}
+              />
+            ))}
+          </>
+        ) : (
+          <div className="empty_cart">
+            <h2>Your cart is empty</h2>
+            <p>
+              Your shopping cart is waiting. Give it purpose – fill it with
+              groceries, clothing, household supplies, electronics and more.
+              Continue shopping on the Amazon.in homepage, learn about today's
+              deals, or visit your Wish List.
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="checkout__right">
         <Subtotal />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useStateValue } from "./StateProvider";
+import "./CheckoutProduct.css";
+
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
+    const [{ basket }, dispatch] = useStateValue();
+    const removeFromBasket = () =>{
+        dispatch({
+            type:'REMOVE_FROM_BASKET',
+            id,
+        })
+    }
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" alt="" src={image} />
+      <div className="checkoutProduct__info">
+        <div className="content">
+          <p className="checkoutProduct__title">{title}</p>
+          <div className="checkoutProduct__price">
+            <div style={{ float: "right" }}>
+              <small>₹ </small>
+              <strong>{price}</strong>
+            </div>
+          </div>
+        </div>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+        <button onClick={removeFromBasket} className="checkoutProduct__button">Remove from basket</button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProduct;
